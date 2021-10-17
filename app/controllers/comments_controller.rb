2175d0e38cb28464c02b0f5218a4547e0324e9d0
Kevin Chen <chen.kcw@gmail.com>
2021-10-17 16:27:56 -0700

    TDD #create for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find params[:post_id]
+    @comment = Comment.new params.require(:comment).permit(:body)
+    @comment.post = @post
+    if @post.save
+      redirect_to post_path(@post), notice: "Comment submitted"
+    else
+      @comments = @post.comments.order(created_at: :desc)
+      render 'post/show'
+    end
+  end
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-
+  describe "#create" do
+    it "should add a comment record in db" do
+      before_count = Comment.count
+      p = FactoryBot.create(:post)
+      c = FactoryBot.create(:comment, post: p)
+      after_count = Comment.count
+      expect(after_count).to(eq(before_count + 1))
+    end
+  end
 end
