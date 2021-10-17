caf00c5047a58978105ca9f8dd55b90ba96b82d2
Kevin Chen <chen.kcw@gmail.com>
2021-10-16 20:07:05 -0700

    TDD add validates title should be unique for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe "validates" do
-    it "should have a title" do
-      p = FactoryBot.build(:post, title: nil)
-      p.valid?
-      expect(p.errors.messages).to(have_key(:title))
+    describe "title" do
+      it "should have a title" do
+        p = FactoryBot.build(:post, title: nil)
+        p.valid?
+        expect(p.errors.messages).to(have_key(:title))
+      end
+      it "should be unique" do
+        persisted_p = FactoryBot.create(:post)
+        p = FactoryBot.build(:post, title: persisted_p.title)
+        p.valid?
+        expect(p.errors.messages).to(have_key(:title))
+      end
     end
   end
 end
