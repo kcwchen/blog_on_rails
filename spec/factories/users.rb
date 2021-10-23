9a9e808cfba23087d32d37053315887037846d27
Kevin Chen <chen.kcw@gmail.com>
2021-10-22 20:55:14 -0700

    TDD validates email should be unique

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
-    password_digest { "MyString" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    sequence(:email) {|n| "random-email-#{n}@gmail.com"}
+    password { "123" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validates" do
+    describe "email" do
+      it "should be unique" do
+        persisted_u = FactoryBot.create(:user)
+        u = FactoryBot.build(:user, email: persisted_u.email)
+        u.valid?
+        expect(u.errors.messages).to(have_key(:email))
+      end
+    end
+  end
 end
