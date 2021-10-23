3cb17ee1be733945168a6bf68c9c655e45bae0b8
Kevin Chen <chen.kcw@gmail.com>
2021-10-23 14:53:59 -0700

    Add association between user and posts and user and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
   validates :email, uniqueness: true
   validates :first_name, :last_name, :email, :password, presence: :true
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
