991d6ad5786d6cdd8f89d749ecf8bd26b8e4c5b4
Kevin Chen <chen.kcw@gmail.com>
2021-10-24 10:04:05 -0700

    User can change name and email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
@@ -18,15 +17,30 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      render :edit
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(
+    user_params = params.require(:user).permit(
       :first_name,
       :last_name,
       :email,
       :password,
       :password_confirmation
     )
+
+    # Remove the password and password confirmation keys for empty values
+    #user_params.delete(:password) unless user_params[:password].present?
+    #user_params.delete(:password_confirmation) unless user_params[:password_confirmation].present?
+
+    user_params
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates :email, uniqueness: true
-  validates :first_name, :last_name, :email, :password, presence: :true
+  validates :first_name, :last_name, :email, presence: :true
+  validates :password, presence: :true, :if => :password
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
   def full_name
