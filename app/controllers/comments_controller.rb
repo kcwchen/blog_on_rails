04fa803e1e552fbeccbc59f6a1544714e6ad755b
Kevin Chen <chen.kcw@gmail.com>
2021-10-23 21:28:02 -0700

    Edit user form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,11 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     @post = Post.find params[:post_id]
     @comment = Comment.new params.require(:comment).permit(:body)
     @comment.post = @post
+    @comment.user = current_user
     if @comment.save
       redirect_to post_path(@post), notice: "Comment submitted"
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = current_user
+  end
+
   private
 
   def user_params
