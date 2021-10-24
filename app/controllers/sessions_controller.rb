8c6ab9f907ad1453f400cba49bd4a773be5bd1b4
Kevin Chen <chen.kcw@gmail.com>
2021-10-23 20:55:25 -0700

    Session create action and sign in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    @user = User.find_by_email params[:email]
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Signed In"
+    else
+      flash.now[:alert] = "Wrong Email or Password"
+      render :new
+    end
   end
 end
