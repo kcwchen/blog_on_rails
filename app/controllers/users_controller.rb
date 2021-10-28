f70c5ed2d08d1c0b106ded30ed7fbb1fbcc5113e
Kevin Chen <chen.kcw@gmail.com>
2021-10-28 14:59:38 -0700

    Update password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
   def update
     @user = current_user
     if @user.update(user_params)
+      flash.now[:notice] = "Account Updated"
       render :edit
     else
       render :edit
@@ -32,10 +33,28 @@ class UsersController < ApplicationController
 
   def update_password
     @user = current_user
-    if @user.update(user_params)
-      render :edit
+    if @user&.authenticate(params[:current_password])
+      current_password = params[:current_password]
+      new_password = params[:new_password]
+      new_password_confirmation = params[:new_password_confirmation]
+      if new_password != new_password_confirmation
+        flash[:alert] = "New password does not match new password confirmation"
+        render :edit_password
+      elsif new_password == current_password
+        flash[:alert] = "New password is the same as current password"
+        render :edit_password
+      else
+        if @user.update(password: new_password, password_confirmation: new_password_confirmation)
+          flash[:notice] = "Password Updated"
+          render :edit
+        else
+          flash[:alert] = "Password could not be updated"
+          render :edit_password
+        end
+      end
     else
-      render :edit
+      flash[:alert] = "Current password is incorrect"
+      render :edit_password
     end
   end
 
